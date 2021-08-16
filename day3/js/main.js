4ea4f23049bc6cad8bf99253fb69021c2ef5d719
aravinds-arv <aravinds.arv@gmail.com>
2021-08-16 21:46:39 +0530

    day-3

---
+++ day3/js/main.js
@@ -0,0 +1,16 @@
+let a = 0
+
+function add(){
+    a++
+    document.getElementById('screen').innerText = a
+}
+
+function sub(){
+    a--
+    document.getElementById('screen').innerText = a
+}
+
+function res(){
+    a = 0
+    document.getElementById('screen').innerText = a
+}
