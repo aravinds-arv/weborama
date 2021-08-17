cb224c7c9b3e652b3807ea97192b919f7819e208
aravinds-arv <aravinds.arv@gmail.com>
2021-08-17 19:58:51 +0530

    day4

---
+++ day4/main.js
@@ -0,0 +1,17 @@
+let a  = 10
+a = 10
+console.log(a)
+
+function display(){
+    console.log("HI There!")
+}
+
+let display1=()=>{
+    console.log("Hello There!")
+}
+
+display()
+display()
+display()
+display1()
+display1()
